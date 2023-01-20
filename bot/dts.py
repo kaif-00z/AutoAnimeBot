b7d7a468f996f33311a4993a74a9b29083f05efb
kaif-00z <88398455+kaif-00z@users.noreply.github.com>
2023-01-20 23:06:53 +0530

    fixes and typos

--- bot/dts.py
+++ bot/dts.py
@@ -1,3 +1,19 @@
+#    This file is part of the AutoAnime distribution.
+#    Copyright (c) 2023 Kaif_00z
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/kaif-00z/AutoAnimeBOt/blob/main/LICENSE > .
+
+
 import json
 
 import aiohttp
