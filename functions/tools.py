3230651ea51047e9040f1ecd8e0aab9c99aaa4c7
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-19 13:24:11 +0530

    Update tools.py

--- functions/tools.py
+++ functions/tools.py
@@ -143,7 +143,7 @@ class Tools:
         return True, out
 
     async def compress(self, dl, out):
-        cmd = f'''{Var.FFMPEG} -i """{dl}""" -metadata:s:v:0 title="[Anime Compass] : (This Episode)" -metadata:s:a:0 title="[Telegram: @Anime_Compass]" -metadata:s:a:1 title="[Telegram: @Anime_Compass]" -map 0:v? -map 0:a? -map 0:s? -map 0:t? -metadata title="@Anime_Compass" -metadata author="@Anime_Compass" -metadata:s:s title="@Anime_Compass" -metadata:s:a title="@Anime_Compass" -metadata:s:v title="@Anime_compass" -c:v libx265 -pix_fmt yuv420p10le -preset veryfast  -crf 27 -c:a libopus -ac 2 -vbr 2 -ab 64k -c:s copy -movflags +faststart -vf "drawtext=fontfile=Imprima_Love.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:text=Anime Compass" """{out}""" -y'''
+        cmd = f'''{Var.FFMPEG} -i """{dl}""" -metadata:s:v:0 title="[Anime Compass] : (This Episode)" -metadata:s:a:0 title="[Telegram: @Anime_Compass]" -metadata:s:a:1 title="[Telegram: @Anime_Compass]" -map 0:v? -map 0:a? -map 0:s? -map 0:t? -metadata title="@Anime_Compass" -metadata author="@Anime_Compass" -metadata:s:s title="@Anime_Compass" -metadata:s:a title="@Anime_Compass" -metadata:s:v title="@Anime_compass" -c:v libx265 -preset ultrafast -crf 27 -c:a libopus -ac 2 -vbr 2 -ab 64k -c:s copy -movflags +faststart -vf "drawtext=fontfile=Imprima_Love.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:text=Anime Compass" """{out}""" -y'''
         process = await asyncio.create_subprocess_shell(
             cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
