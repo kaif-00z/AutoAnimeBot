e385c8934192208ef110156451846df2a3393e0b
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-21 19:03:08 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -44,14 +44,7 @@ class DataBase:
         self.cache = {}
         self.re_cache()
 
-    def ask_(db: Redis):
-        import sys 
     
-        if "--newdb" in sys.argv:
-            db.flushall()
-        elif "--samedb" in sys.argv or os.getenv("FLUSH_DATABASE", "").lower() == "y":
-            db.flushall()
-            LOGS.info("Successfully Flushed The Database!!!")
         
     def add_anime(self, name):
         data = self.cache.get("ANIMES_UPLOADED") or []
