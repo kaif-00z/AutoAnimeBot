d6f17331e69cbfe5d0d63b8ff6de577e8ead48f5
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-21 13:27:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,6 +51,7 @@ async def _start(event):
     msg_id = event.pattern_match.group(1)
     xnx = await event.reply("`Please Wait...`")
     if Var.FORCESUB_CHANNEL and Var.FORCESUB_CHANNEL_LINK and Var.FORCESUB_CHANNEL_2 and Var.FORCESUB_CHANNEL_LINK_2:
+    if Var.FORCESUB_CHANNEL and Var.FORCESUB_CHANNEL_LINK and Var.FORCESUB_CHANNEL_2 and Var.FORCESUB_CHANNEL_LINK_2:
         is_user_joined = await bot.is_joined(Var.FORCESUB_CHANNEL, event.sender_id)
         is_user_joined_2 = await bot.is_joined(Var.FORCESUB_CHANNEL_2, event.sender_id)
     if not is_user_joined or not is_user_joined_2:
@@ -77,10 +78,6 @@ async def _start(event):
                 "♻️ REFRESH",
                 url=f"https://t.me/{((await bot.get_me()).username)}?start={msg_id}",
             )
-        )
-        return await xnx.edit(
-            message,
-            buttons=buttons
         )
         return await xnx.edit(
             message,
