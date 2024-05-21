c516d9210deae27cca0574682a6275dc255ce6ac
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-21 13:32:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,6 @@ async def _start(event):
     msg_id = event.pattern_match.group(1)
     xnx = await event.reply("`Please Wait...`")
     if Var.FORCESUB_CHANNEL and Var.FORCESUB_CHANNEL_LINK and Var.FORCESUB_CHANNEL_2 and Var.FORCESUB_CHANNEL_LINK_2:
-    if Var.FORCESUB_CHANNEL and Var.FORCESUB_CHANNEL_LINK and Var.FORCESUB_CHANNEL_2 and Var.FORCESUB_CHANNEL_LINK_2:
         is_user_joined = await bot.is_joined(Var.FORCESUB_CHANNEL, event.sender_id)
         is_user_joined_2 = await bot.is_joined(Var.FORCESUB_CHANNEL_2, event.sender_id)
     if not is_user_joined or not is_user_joined_2:
