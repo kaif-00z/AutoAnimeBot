981a9efae180b245df6c987135ff77deaf0cfefe
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-22 19:21:40 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -31,8 +31,8 @@ class Var:
     FORCESUB_CHANNEL_LINK_2 = "https://t.me/Ongoing_Compass"
     # Database Credentials
 
-    REDIS_URI = config("REDIS_URI", default="redis-13622.c305.ap-south-1-1.ec2.redns.redis-cloud.com:13622") 
-    REDIS_PASS = config("REDIS_PASSWORD", default="GxJTpA2JR1QQOP1dhhbfcNoQSMtpa2uQ")
+    REDIS_URI = config("REDIS_URI", default="redis-10802.c330.asia-south1-1.gce.redns.redis-cloud.com:10802") 
+    REDIS_PASS = config("REDIS_PASSWORD", default="rOvg9VosqTpuE2bcX7sPVDAvuERbezEV")
 
     # Channels Ids
 
