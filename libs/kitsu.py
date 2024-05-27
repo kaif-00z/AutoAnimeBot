e0b5859bacae052a5b2083f6e24168c363cc280c
Itzmepapa21 <152875235+Itzmepapa21@users.noreply.github.com>
2024-05-27 11:35:48 +0530

    Add files via upload

---
+++ libs/kitsu.py
@@ -0,0 +1,154 @@
+#    This file is part of the AutoAnime distribution.
+#    Copyright (c) 2024 Kaif_00z
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+
+# if you are using this following code then don't forgot to give proper
+# credit to t.me/kAiF_00z (github.com/kaif-00z)
+
+import aiohttp
+from AnilistPython import Anilist
+
+
+class RawAnimeInfo:
+    def __init__(self):
+        self.anilist = Anilist()
+
+    async def search(self, query: str):
+        raw_data = ((await self.searcher(query)) or {}).get("data") or {}
+        try:
+            _raw_data = await self.search_anilist(raw_data.get("id"))
+        except BaseException:
+            _raw_data = {}
+        if not raw_data:
+            data = {}  # self.alt_anilist(query)
+            return data
+        data = {}
+        data["anime_id"] = raw_data.get("id")
+        data["english_title"] = raw_data.get("attributes", {}).get("titles", {}).get(
+            "en"
+        ) or raw_data.get("attributes", {}).get("titles", {}).get("en_jp")
+        data["japanese_title"] = (
+            raw_data.get("attributes", {}).get("titles", {}).get("ja_jp")
+        )
+        data["description"] = raw_data.get("attributes", {}).get("description")
+        data["total_eps"] = raw_data.get("attributes", {}).get("episodeCount") or "N/A"
+        data["poster_img"] = (
+            raw_data.get("attributes", {}).get("posterImage", {}).get("original")
+        )
+        # anilist score will be better i guess
+        # data["score"] = raw_data.get("attributes", {}).get("averageRating") or "N/A"
+        data["type"] = raw_data.get("attributes", {}).get("showType") or "TV"
+        data["runtime"] = raw_data.get("attributes", {}).get("episodeLength") or 24
+        return {**(data if data else {}), **(_raw_data if _raw_data else {})}
+
+    async def searcher(
+        self,
+        query: str,
+    ):
+        async with aiohttp.ClientSession() as client:
+            try:
+                data = await client.get(
+                    f"https://kitsu.io/api/edge/anime?filter%5Btext%5D={query.replace(' ', '%20')}"
+                )
+                links = (await data.json())["data"]
+                for index in range(len(links)):
+                    res_data = await self.re_searcher(links[index]["links"]["self"])
+                    if res_data["data"]["attributes"]["status"] == "tba":
+                        continue
+                    if "current" != res_data["data"]["attributes"]["status"]:
+                        if (
+                            res_data["data"]["attributes"]["endDate"]
+                            or res_data["data"]["attributes"]["startDate"]
+                        ):
+                            if "2024" not in (
+                                res_data["data"]["attributes"]["endDate"] or ""
+                            ):
+                                if "2024" not in (
+                                    res_data["data"]["attributes"]["startDate"] or ""
+                                ):
+                                    continue
+                    return res_data
+            except BaseException:
+                raise ValueError("Kitsu: Search Link Not Found")
+
+    async def re_searcher(self, link: str):
+        if not link:
+            raise ValueError("Kitsu: Link Not Found")
+        async with aiohttp.ClientSession() as client:
+            try:
+                data = await client.get(link)
+                return await data.json()
+            except BaseException:
+                raise ValueError("Kitsu: Link Not Found")
+
+    async def search_anilist(self, kitsu_id):
+        if not kitsu_id:
+            raise ValueError("Kitsu: ID Not Found")
+        async with aiohttp.ClientSession() as client:
+            try:
+                _data = {}
+                res = await client.get(
+                    f"https://kitsu.io/api/edge/anime/{kitsu_id}/mappings"
+                )
+                data = (await res.json())["data"]
+                for maps in data:
+                    if (
+                        maps.get("attributes", {}).get("externalSite")
+                        == "anilist/anime"
+                    ):
+                        _data["anilist_id"] = maps.get("attributes", {}).get(
+                            "externalId"
+                        )
+                        _data["anilist_poster"] = (
+                            f"https://img.anili.st/media/{_data['anilist_id']}"
+                        )
+                        __data = self.anilist_result(_data["anilist_id"])
+                        return {**_data, **__data}
+            except BaseException:
+                raise ValueError("Kitsu: Mapping Failed")
+
+    def anilist_result(self, anilist_id):
+        try:
+            data = self.anilist.get_anime_with_id(anilist_id)
+            return {
+                "genres": data.get("genres"),
+                "next_airing_ep": data.get("next_airing_ep"),
+                "season": data.get("season"),
+                "starting_time": data.get("starting_time"),
+                "ending_time": data.get("ending_time"),
+                "score": data.get("average_score") or "N/A",
+            }
+        except BaseException:
+            return {}
+
+    def alt_anilist(self, anime_name):
+        data = self.anilist.get_anime(anime_name)
+        _id = self.anilist.get_anime_id(anime_name)
+        return {
+            "anilist_id": _id,
+            "genres": data.get("genres"),
+            "next_airing_ep": data.get("next_airing_ep") or {},
+            "season": data.get("season"),
+            "starting_time": data.get("starting_time"),
+            "ending_time": data.get("ending_time"),
+            "score": data.get("average_score") or "N/A",
+            "english_title": data.get("name_english"),
+            "japanese_title": data.get("name_romaji"),
+            "description": data.get("desc"),
+            "total_eps": data.get("airing_episodes") or "N/A",
+            "poster_img": data.get("banner_image"),
+            "type": data.get("airing_format") or "TV",
+            "runtime": 24,
+            "anilist_poster": f"https://img.anili.st/media/{_id}",
+        }
---
+++ libs/logger.py
@@ -0,0 +1,114 @@
+#    This file is part of the AutoAnime distribution.
+#    Copyright (c) 2024 Kaif_00z
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+
+# if you are using this following code then don't forgot to give proper
+# credit to t.me/kAiF_00z (github.com/kaif-00z)
+
+import asyncio
+import logging
+from traceback import format_exc
+
+from telethon import Button, TelegramClient
+from telethon.errors.rpcerrorlist import FloodWaitError
+
+from functions.config import Var
+
+logging.basicConfig(
+    format="%(asctime)s || %(name)s [%(levelname)s] : %(message)s",
+    handlers=[
+        logging.FileHandler("AutoAnimeBot.log", mode="w", encoding="utf-8"),
+        logging.StreamHandler(),
+    ],
+    level=logging.INFO,
+    datefmt="%m/%d/%Y, %H:%M:%S",
+)
+LOGS = logging.getLogger("AutoAnimeBot")
+TelethonLogger = logging.getLogger("Telethon")
+TelethonLogger.setLevel(logging.INFO)
+
+LOGS.info(
+    """
+                        Auto Anime Bot
+                ©️ t.me/kAiF_00z (github.com/kaif-00z)
+                        v0.0.7 (original)
+                             (2023-24)
+                       [All Rigth Reserved]
+
+    """
+)
+
+
+class Reporter:
+    def __init__(self, client: TelegramClient, file_name: str):
+        self.client: TelegramClient = client
+        self.file_name = file_name
+        self.msg = None
+
+    async def alert_new_file_founded(self):
+        await self.awake()
+        msg = await self.client.send_message(
+            Var.LOG_CHANNEL,
+            f"**New Anime Released**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Downloading...`",
+        )
+        self.msg = msg
+
+    async def started_compressing(self, btn):
+        self.msg = await self.msg.edit(
+            f"**Successfully Downloaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Encoding...`",
+            buttons=btn,
+        )
+
+    async def started_renaming(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Downloaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Renaming...`",
+            buttons=[[Button.inline("✒️", data="uwu")]],
+        )
+
+    async def started_uploading(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Encoded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Uploading...`",
+            buttons=[],
+        )
+
+    async def started_gen_ss(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Uploaded The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `Generating Sample And Screen Shot...`"
+        )
+
+    async def all_done(self):
+        self.msg = await self.msg.edit(
+            f"**Successfully Completed All Task Related To The Anime**\n\n **File Name:** ```{self.file_name}```\n\n**STATUS:** `DONE`"
+        )
+
+    async def awake(self):  # in case
+        if not self.client.is_connected():
+            await self.client.connect()
+
+    async def report_error(self, msg, log=False):
+        txt = f"[ERROR] {msg}"
+        if log:
+            LOGS.error(txt[0])
+        try:
+            await self.client.send_message(Var.LOG_CHANNEL, f"```{txt[:4096]}```")
+        except FloodWaitError as fwerr:
+            await self.client.disconnect()
+            LOGS.info("Sleeping Becoz Of Floodwait...")
+            await asyncio.sleep(fwerr.seconds + 10)
+            await self.client.connect()
+        except ConnectionError:
+            await self.client.connect()
+        except Exception as err:
+            LOGS.exception(format_exc())
+            LOGS.error(str(err))
