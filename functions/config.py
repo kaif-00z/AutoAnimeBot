a46a28dccf9bce1a85b029c56b0201f02b67bd34
Kaif K. <88398455+kaif-00z@users.noreply.github.com>
2024-08-26 15:30:43 +0530

    fixes

--- functions/config.py
+++ functions/config.py
@@ -52,4 +52,5 @@ class Var:
     CRF = config("CRF", default="27")
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
+    LOG_ON_MAIN = config("LOG_ON_MAIN", default=False, cast=bool)
     FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="", cast=str)
