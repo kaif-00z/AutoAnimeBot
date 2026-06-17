4814070a9b9cf81590fc2ca87522d48aa81234e3
kAiF <kaif-00z@proton.me>
2026-06-17 13:04:43 +0000

    fix: `rss_feed_data` is an async func
    - ah it should be async, it was just out of my mind at that time (nvm)

--- libs/subsplease.py
+++ libs/subsplease.py
@@ -44,7 +44,7 @@ class SubsPlease:
             LOGS.error(format_exc())
         sys.exit(0)
 
-    def rss_feed_data(self):
+    async def rss_feed_data(self):
         try:
             loop = asyncio.get_event_loop()
             d1080, d720, d480 = await asyncio.gather(
